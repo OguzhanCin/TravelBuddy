4418b3c7b66be210ddcda143c9c27288d060002d
OguzhanCin <oguzhanberkcin@gmail.com>
2025-01-30 02:16:13 +0300

    The search page was created and edited in search_screen.dart.

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import '../core/themes.dart';
+import '../widgets/navigationbar.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: colors["primary"],
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            title: TextField(
+              decoration: InputDecoration(
+                hintText: "Ara...",
+                focusColor:Theme.of(context).colorScheme.onSecondary),
+                filled: true,
+                fillColor: Theme.of(context).colorScheme.surfaceVariant,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                  borderSide: BorderSide.none,
+                ),
+                prefixIcon: Icon(Icons.search,
+                    color: Theme.of(context).colorScheme.onSecondary),
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: EdgeInsets.all(4),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                childAspectRatio: 1,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.primary,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(7),
+                    child: Image.asset(
+                      "assets/images/gonderi${index + 1}.jpg",
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Icon(
+                          Icons.photo,
+                          size: 100,
+                          color: Theme.of(context).colorScheme.onSecondary,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                childCount: 9,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomMenu(),
+    );
+  }
+}
